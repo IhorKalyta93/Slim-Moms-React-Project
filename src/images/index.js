ca6097f8e0879b649290557b21756828a986e549
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-04-01 23:54:15 +0200

    1.2

---
+++ src/images/index.js
@@ -0,0 +1,51 @@
+import BananaDesktop from './Banana/BananDekstop.webp';
+import BananaTablet from './Banana/BananTablet.webp';
+
+import LeafDesk from './Leaf/LeafDesk.webp';
+import LeafFooterTablet from './Leaf/LeafFoterTablet.webp';
+import LeafPageRegister from './Leaf/LeafPageRegst.webp';
+import LeafSideBar from './Leaf/LeafSideBar.webp';
+import LeafTablet from './Leaf/LeafTablet.webp';
+
+import StrawberryDesk from './Stravbery/StrawberryDesk.webp'
+import StrawberryTablet from './Stravbery/StrawberryTablet.webp'
+
+import VectorTablet from  './VectorBackground/VectorTablet.webp';
+import VectorDesk from  './VectorBackground/VectorDesk.webp';
+
+import Watermelon from './loader/watermelon.webp'
+import Banana from './loader/banan.webp'
+import Apple from './loader/apple.webp'
+import Avocado from './loader/avokado.webp'
+import Kiwi from './loader/kivi.webp'
+import Lemon from './loader/lemon.webp'
+import Lime from './loader/lime.webp'
+import Mandarin from './loader/mandarin.webp'
+import Grapefruit from './loader/greyp.webp'
+import Orange from './loader/orange.webp'
+
+
+export  {
+  BananaDesktop,
+  BananaTablet,
+  LeafDesk,
+  LeafFooterTablet,
+  LeafPageRegister,
+  LeafSideBar,
+  LeafTablet,
+  StrawberryDesk,
+  StrawberryTablet,
+  VectorTablet,
+  VectorDesk,
+  Watermelon,
+  Banana,
+  Apple,
+  Avocado,
+  Kiwi,
+  Lemon,
+  Lime,
+  Mandarin,
+  Grapefruit,
+  Orange
+}
+
