29e29219d8e81263af23c0d157ec297272b00dbc
Ihor Kalyta <ihor.kalyta93@gmail.com>
2023-04-25 18:43:02 +0200

    1.9.9

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { persistor, store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Slim-Moms-React-Project/">
+    <BrowserRouter basename="Slim-Moms-React-Project">
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <ChakraProvider theme={customTheme}>
